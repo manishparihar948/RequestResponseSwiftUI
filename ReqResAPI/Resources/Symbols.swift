b3d38e41f04109cfaa17bb62f7ee6459658b838f
manishparihar948 <manish.parihar948@gmail.com>
2023-10-30 09:09:14 +0100

    Added Assests

---
+++ ReqResAPI/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  ReqResAPI
+//
+//  Created by Manish Parihar on 30.10.23.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let refresh = Image(systemName: "arrow.clockwise")
+    static let checkmark = Image(systemName: "checkmark")
+    static let link = Image(systemName: "link")
+    static let plus = Image(systemName: "plus")
+}
---
+++ ReqResAPI/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  ReqResAPI
+//
+//  Created by Manish Parihar on 30.10.23.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detailBackground")
+    static let text = Color("text")
+    static let star = Color("star")
+}
