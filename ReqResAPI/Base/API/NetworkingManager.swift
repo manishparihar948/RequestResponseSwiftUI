3eaf4d371494854ee97b3c18dede6a0dfe620aa1
manishparihar948 <manish.parihar948@gmail.com>
2023-10-31 08:30:20 +0100

    Added networking manager singleton class for accessing api response on people view

---
+++ ReqResAPI/Base/API/NetworkingManager.swift
@@ -0,0 +1,67 @@
+//
+//  NetworkingManager.swift
+//  ReqResAPI
+//
+//  Created by Manish Parihar on 31.10.23.
+//
+
+import Foundation
+
+final class NetworkingManager {
+    
+    static let shared = NetworkingManager()
+    
+    private init() {}
+    
+    func request<T: Codable>(_ absoluteURL: String,
+                             type: T.Type,
+                             completion: @escaping (Result<T, Error>) -> Void) {
+        
+        guard let url = URL(string: absoluteURL) else {
+            completion(.failure(NetworkingError.invalidUrl))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if error != nil {
+                completion(.failure(NetworkingError.custom(error: error!)))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200...300) ~= response.statusCode else {
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                completion(.failure(NetworkingError.invalidStatusCode(statusCode: statusCode)))
+                      return
+                  }
+            
+            guard let data = data else {
+                completion(.failure(NetworkingError.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let res = try decoder.decode(T.self, from: data)
+                completion(.success(res))
+            } catch  {
+                print(error)
+            }
+        }
+        dataTask.resume()
+    }
+}
+
+extension NetworkingManager {
+    enum NetworkingError: Error {
+        case invalidUrl
+        case custom(error: Error)
+        case invalidStatusCode(statusCode: Int)
+        case invalidData
+        case failedToDecode(error: Error)
+    }
+}
